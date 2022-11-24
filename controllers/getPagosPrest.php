087cac88bb123249ab110a348840b273a9cf6d54
JPCHIPRES <Jchipres@ucol.mx>
2022-11-24 09:47:59 -0600

    bug lista pagos √

--- controllers/getPagosPrest.php
+++ controllers/getPagosPrest.php
@@ -2,7 +2,7 @@
 require __DIR__ .'\..\includes\db.php';
 session_start();
 $id_cliente=$_POST['id_cliente'];
-$records = $conn->prepare('SELECT DATE(pagos.fecha_hora) as fecha, TIME(pagos.fecha_hora) as hora, pagos.monto, pagos.id_prestamo, pagos.id_pago FROM cuenta INNER JOIN pagos where cuenta.id_cliente= '.$id_cliente.' ORDER BY fecha_hora');
+$records = $conn->prepare('SELECT DATE(pagos.fecha_hora) as fecha, TIME(pagos.fecha_hora) as hora, pagos.monto, pagos.id_prestamo, pagos.id_pago FROM cuenta INNER JOIN pagos on cuenta.numCta= pagos.numCta where cuenta.id_cliente= '.$id_cliente.' ORDER BY fecha_hora');
 $records->execute();
 $transaccion = $records->fetchAll(PDO::FETCH_ASSOC);
 if($records->rowCount() > 0){
